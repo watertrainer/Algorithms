90bbfbbc291872692cadc256459421d18dc9ce11
watertrainer <47402928+watertrainer@users.noreply.github.com>
2019-11-13 09:46:20 +0100

    Programmed Selection Sort, with visualisation

--- Sorting/Klasse1/src/SelectionSort.java
+++ Sorting/Klasse1/src/SelectionSort.java
@@ -1,5 +1,51 @@
-public class SelectionSort {
-    public static void main(String[] args){
+package SelectionSort;
 
-    }
+import java.awt.Graphics;
+import java.util.Arrays;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SelectionSort {
+	public static int[] sort(int[] ar) {
+		JFrame f = new JFrame();
+		JPanel p = new JPanel() {
+			@Override
+			public void paintComponent(Graphics g) {
+				for(int i = 0;i<ar.length;i++) {
+					g.drawRect(5*i, 0, 5, ar[i]);
+				}
+			}
+		};
+		f.setContentPane(p);
+		f.setSize(500, 500);
+		f.setVisible(true);
+		int s = 0;
+		int u = ar.length;
+		while(u>0) {
+			for(int i = s+1;i<ar.length;i++) {
+				if(ar[s]>ar[i]) {
+					int t = ar[s];
+					ar[s] = ar[i];
+					ar[i] = t;
+				}
+				
+			}
+			u--;
+			s++;
+			f.repaint();
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return ar;
+	}
+	
+	public static void main(String[] args) {
+	
+		System.out.println(Arrays.toString(sort(new int[] {3,5,9,12,352,10,13,10,1,23,1,34,456,123,45,23,123,234,23,23,34,234})));
+	}
 }
