076fe13586b57209b030db3f06e48e53e81e8ec7
watertrainer <watertrainer42@gmail.com>
2019-11-26 22:07:56 +0100

    Quicksort start and finish

---
+++ Sorting/Klasse3/Bucketsort.java
@@ -0,0 +1,10 @@
+public class Bucketsort {
+    public static void main(String[] args){
+        int ar[] = new int[]{1,2,3,4,5,6,2,8,9};
+
+    }
+
+    public static void sort(int[] ar, int k){
+
+    }
+}
