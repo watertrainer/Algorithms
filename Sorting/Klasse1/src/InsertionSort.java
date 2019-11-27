a44b0ef1900c42d5ad6600151e7dc93c4dc9cf51
joejosy <joejosy@192.168.97.133>
2019-11-27 12:19:16 +0100

    insertionSort
    sollte verständlich sein, bei Fragen whatsApp...joa.

--- Sorting/Klasse1/src/InsertionSort.java
+++ Sorting/Klasse1/src/InsertionSort.java
@@ -1,6 +1,28 @@
 public class InsertionSort {
 
-int c;
-int g;
+	public static void main(String[] args) {
+
+	int[] unsortiert = { 5, 15, 7, 42, 6, 23 };
+	int[] sortiert = insertionSort(unsortiert);
+
+	for (int i = 0; i < sortiert.length; i++) {
+		System.out.print(sortiert[i] + ", ");
+	}
+
 }
 
+public static int[] insertionSort(int[] sortieren) {
+	int temp;
+	for (int i = 1; i < sortieren.length; i++) {
+		temp = sortieren[i];
+		int j = i;
+		while (j > 0 && sortieren[j - 1] > temp) {
+			sortieren[j] = sortieren[j - 1];
+			j--;
+		}
+		sortieren[j] = temp;
+	}
+	return sortieren;
+}
+
+}
