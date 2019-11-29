5b8f3037b55c25b0d14b4d9dc5f5e4593a462b9f
Alejavandro <alepach@gmx.de>
2019-11-29 15:14:53 +0100

    BubbleSort Clean-up

--- Sorting/Klasse1/src/BubbleSort.java
+++ Sorting/Klasse1/src/BubbleSort.java
@@ -8,7 +8,7 @@ public class BubbleSort {
 
 
     public void sortieren(){
-        for(int o = 1; o <= arr.length-1; o++) {
+        for(int j = 0; j <= arr.length-2; j++) {
             for (int i = 0; i <= arr.length - 2; i++) {
 
                 if (arr[i] > arr[i + 1]) {
