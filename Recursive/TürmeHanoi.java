86deda007599a6e9fce4c20be38be0e4aebfbc71
Alejavandro <alepach@gmx.de>
2020-01-29 12:25:16 +0100

    Türme HAnoi fertig

---
+++ Recursive/TürmeHanoi.java
@@ -0,0 +1,18 @@
+public class TürmeHanoi {
+
+    public static void tower(int n, char start, char end, char aux){
+
+        if(n == 1){
+            System.out.println("Bewege die Scheibe 1 von " + start + " nach " + end);
+            return;
+        }
+        tower(n - 1, start, aux, end);
+        System.out.println("Bewege Scheibe " + n + " von " + start + " nach " + end);
+        tower(n-1, aux, end, start);
+    }
+
+    public static void main(String[] args){
+
+        tower(4, 'A', 'C', 'B');
+    }
+}
