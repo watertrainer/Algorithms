b62f2b34f95e51b9f46c2d8074ffbd4bdadb1bb2
Alejavandro <alepach@gmx.de>
2020-02-05 11:53:56 +0100

    Tiefensuche ist fertig

--- Searching/Tiefenbreitensuche.java
+++ Searching/Tiefenbreitensuche.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Tiefenbreitensuche {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Knoten eins = new Knoten(1);
         Knoten drei = new Knoten(3);
@@ -12,7 +12,6 @@ public class Tiefenbreitensuche {
         Knoten zehn = new Knoten(10);
         Knoten neun = new Knoten(9);
         Knoten acht = new Knoten(8);
-
 
         eins.kinder.add(zwei);
         eins.kinder.add(drei);
@@ -24,24 +23,26 @@ public class Tiefenbreitensuche {
 
         Knoten re = tiefenSuche(eins, sechs);
         System.out.println("Der Wert " + re.wert + " wurde zurückgegeben!");
-
     }
 
     static ArrayList<Knoten> stack = new ArrayList<>();
 
-    public static Knoten tiefenSuche(Knoten node, Knoten ziel){
+    public static Knoten tiefenSuche(Knoten node, Knoten ziel) {
 
-        if (node == ziel){
+        if (node == ziel) {
+
             System.out.println("Der Wert " + node.wert + " wurde gefunden!");
             return node;
-        }
-        else{
-            stack.addAll(node.kinder);
-            while(!stack.isEmpty()){
-                System.out.println("Der Wert " + node.wert + " wurde besucht!");
-                node = stack.get(stack.size()-1);
 
-                stack.remove(stack.size()-1);
+        } else {
+
+            stack.addAll(node.kinder);
+
+            while (!stack.isEmpty()) {
+
+                System.out.println("Der Wert " + node.wert + " wurde besucht!");
+                node = stack.get(stack.size() - 1);
+                stack.remove(stack.size() - 1);
                 return tiefenSuche(node, ziel);
             }
         }
