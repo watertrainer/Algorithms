4884a13a2274e3081bf2243f4f93909d3ee2ce30
Alejavandro <alepach@gmx.de>
2020-02-18 12:43:20 +0100

    Dijkstra und Breitensuche

---
+++ Graphen/Hellmanford/Algorithm/Dijkstra.java
@@ -0,0 +1,76 @@
+package Algorithm;
+
+import GUI.Graph;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+public class Dijkstra extends GraphAlgorithm {
+
+    public Dijkstra(Graph g){
+        super(g);
+    }
+
+    @Override
+    public void calculateAlgorithm(boolean visual) {
+        //Select starting
+        if (visual)
+            g.info.setText("Starting");
+        int minx = Integer.MAX_VALUE;
+        int maxx = 0;
+        int inx = -1;
+        int inmx = -1;
+        for (int i = 0; i < g.nodes.size(); i++) {
+            if (g.nodes.get(i).x < minx) {
+                minx = g.nodes.get(i).x;
+                inx = i;
+            } else if (g.nodes.get(i).x > maxx) {
+                maxx = g.nodes.get(i).x;
+                inmx = i;
+            }
+        }
+        if (inx == -1 || inmx == -1) {
+            g.info.setText("");
+            return;
+        }
+        g.setStart(g.nodes.get(inx));
+        g.setEnd(g.nodes.get(inmx));
+
+        g.info.setText("Start and End selected. Starting to calculate the shortest Path");
+        List<Node> bekannt = new ArrayList<Node>();
+        Node curr = g.start;
+        Color p = g.select(curr);
+        for(int i = 0; i < g.nodes.size(); i++)
+            g.nodes.get(i).cost = Integer.MAX_VALUE;
+        curr.cost = 0;
+        for(int i = 0; i < curr.edges.size(); i++) {
+            curr.edges.get(i).end.cost = curr.cost + curr.edges.get(i).capacity;
+            bekannt.add(curr.edges.get(i).end);
+        }
+        bekannt.sort(Comparator.comparingInt(a -> a.cost));
+        g.deselect(curr,p);
+        curr = bekannt.get(0);
+        p = g.select(curr);
+        bekannt.remove(0);
+
+        while(!bekannt.isEmpty()){
+            for(int i = 0; i < curr.edges.size(); i++) {
+                curr.edges.get(i).end.cost = curr.cost + curr.edges.get(i).capacity;
+                bekannt.add(curr.edges.get(i).end);
+            }
+            bekannt.sort(Comparator.comparingInt(a -> a.cost));
+
+            if(curr.getEdgeTo(bekannt.get(0))!=null)
+                g.select(curr.getEdgeTo(bekannt.get(0)));
+            curr = bekannt.get(0);
+            p = g.select(curr);
+
+            bekannt.remove(0);
+        }
+        g.info.setText("Shortest Path found. It costs  "+g.end.cost+" to get there");
+        g.sleep(5000);
+    }
+}
---
+++ Searching/Breitensuche.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class Breitensuche {
+
+    public static void main(String[] args) {
+
+        Knoten eins = new Knoten(1);
+        Knoten drei = new Knoten(3);
+        Knoten zwei = new Knoten(2);
+        Knoten sechs = new Knoten(6);
+        Knoten vier = new Knoten(4);
+        Knoten zehn = new Knoten(10);
+        Knoten neun = new Knoten(9);
+        Knoten acht = new Knoten(8);
+
+        eins.kinder.add(zwei);
+        eins.kinder.add(drei);
+        zwei.kinder.add(sechs);
+        drei.kinder.add(vier);
+        drei.kinder.add(zehn);
+        zehn.kinder.add(neun);
+        zehn.kinder.add(acht);
+
+        Knoten re = breitenSuche(eins, acht);
+        System.out.println("Der Wert " + re.wert + " wurde zurückgegeben!");
+    }
+
+    static ArrayList<Knoten> queue = new ArrayList<>();
+
+    public static Knoten breitenSuche(Knoten node, Knoten ziel){
+
+        if (node == ziel){
+
+            System.out.println("Der Wert " + node.wert + " wurde gefunden!");
+            return node;
+
+        } else{
+
+            queue.addAll(node.kinder);
+
+            while(!queue.isEmpty()){
+
+                System.out.println("Der Wert " + node.wert + " wurde besucht!");
+                node = queue.get(0);
+
+                if(node == ziel){
+
+                    System.out.println("Der Wert " + node.wert + " wurde gefunden!");
+                    queue.removeAll(queue);
+                    return node;
+
+                } else {
+                    queue.addAll(node.kinder);
+                    queue.remove(node);
+                }
+            }
+        }
+        return node;
+    }
+}
